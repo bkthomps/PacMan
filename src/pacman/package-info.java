9b284d6ab44e41a476f852321af7534fe4f2fd14
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-04-17 22:50:08 -0400

    Version 0.1.4
    Package description improved.
    
    Please note that the last commit, titled "Version 1.1.3" was intended to be titled "Version 0.1.3", as the program is not yet fully released.

--- src/pacman/package-info.java
+++ src/pacman/package-info.java
@@ -1,8 +1,11 @@
 /**
  * Bailey Thompson <br>
- * Pac-Man (0.1.3) <br>
+ * Pac-Man (0.1.4) <br>
  * 17 April 2017 <br>
  * <br>
- * This is a remake of the popular arcade game, Pac-Man.
+ * This is a remake of the popular arcade game, Pac-Man.&nbsp;The player can navigate the map by using the arrow keys on
+ * the keyboard.&nbsp;To win, the player must eat all yellow points.&nbsp;The player loses if a ghost is touched, except
+ * if the ghost is scared.&nbsp;To make a ghost scared, a big yellow point must be eaten.&nbsp;When a ghost is scared,
+ * it can be eaten for a short period of time, and will respawn in the centre of the board.
  */
 package pacman;
